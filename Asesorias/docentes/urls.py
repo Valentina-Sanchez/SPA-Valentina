07d27801c759cfbcd9902048b12ba6eaaad679a5
Valentina-Sanchez <bvsanchez8@misena.edu.co>
2023-05-27 10:39:57 -0500

    creacion docker

--- Asesorias/docentes/urls.py
+++ Asesorias/docentes/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import Docente_APIView
+from .views import Docente_APIViewGET,Docente_APIViewPOST, Docente_APIView_Detail, Estudiante_APIViewGET, Estudiante_APIViewPOST, Estudiante_APIView_Detail
 
 urlpatterns = [
-   path('v1/doc', Docente_APIView.as_view()) 
+   path('v1/doce/get', Docente_APIViewGET.as_view()),
+   path('v1/doce/post', Docente_APIViewPOST.as_view()),
+   path('v1/doc/<int:pk>', Docente_APIView_Detail.as_view()),
+   path('v1/estu/get', Estudiante_APIViewGET.as_view()),
+   path('v1/estu/post', Estudiante_APIViewPOST.as_view()),
+   path('v1/estu/<int:pk>', Estudiante_APIView_Detail.as_view()),
+
 ]
--- Asesorias/docentes/views.py
+++ Asesorias/docentes/views.py
@@ -10,12 +10,14 @@ from django.http import Http404
 #def listar_estudiante(self, request):
 #   estudiante = Estudiante.objects.filter(pk=1)
 
-class Docente_APIView(APIView):
+class Docente_APIViewGET(APIView):
     def get(self, request, format=None, *args, **kwargs):
         docente = Docente.objects.all()
         serializer = DocenteSerializers(docente, many=True)
 
         return Response(serializer.data)
+    
+class Docente_APIViewPOST(APIView):
     
     def post(self, request, format=None):
        print(request.data)
@@ -24,4 +26,73 @@ class Docente_APIView(APIView):
            serializer.save()
            return Response(serializer.data, status=status.HTTP_201_CREATED)
        return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class Docente_APIView_Detail(APIView):
+    def get_object(self, pk):
+        try: 
+            return Docente.objects.get(pk=pk) 
+        except Docente.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, format=None):
+        docente = self.get_object(pk)
+        serializer = DocenteSerializers(docente)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        docente = self.get_object(pk)
+        serializer = DocenteSerializers(docente, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        docente = self.get_object(pk)
+        docente.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+#Estudiantes
+
+class Estudiante_APIViewGET(APIView):
+    def get(self, request, format=None, *args, **kwargs):
+        estudiante = Estudiante.objects.all()
+        serializer = EstudianteSerializers(estudiante, many=True)
+
+        return Response(serializer.data)
+    
+class Estudiante_APIViewPOST(APIView):
+    
+    def post(self, request, format=None):
+       print(request.data)
+       serializer = EstudianteSerializers(data=request.data) 
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data, status=status.HTTP_201_CREATED)
+       return Response (serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class Estudiante_APIView_Detail(APIView):
+    def get_object(self, pk):
+        try: 
+            return Estudiante.objects.get(pk=pk) 
+        except Estudiante.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, format=None):
+        estudiante = self.get_object(pk)
+        serializer = EstudianteSerializers(estudiante)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        estudiante = self.get_object(pk)
+        serializer = EstudianteSerializers(estudiante, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        estudiante = self.get_object(pk)
+        estudiante.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
