be23c8116d2cdf709b6dae1c18383a738992ff30
Valentina-Sanchez <bvsanchez8@misena.edu.co>
2023-05-26 20:42:56 -0500

    Agregando monolito

--- Asesorias/docentes/views.py
+++ Asesorias/docentes/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-
+from .models import Estudiante, Docente, Asesoria, Materia
 
 # Create your views here.
+
+def listar_estudiante(self, request):
+    estudiante = Estudiante.objects.filter(pk=1)
